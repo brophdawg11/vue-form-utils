9244e876ee28df16df79c5023fb9a3377fcba442
Matt Brophy <matt@brophy.org>
2021-01-31 11:17:43 -0500

    Add cypress

--- test/e2e/WithValidation.e2e.js
+++ test/e2e/WithValidation.e2e.js
@@ -1,98 +1,106 @@
 describe('WithValidation', () => {
 
-    const input = () => cy.get('@iframe').find('[data-test=input]');
-    const info = () => cy.get('@iframe').find('[data-test="info"]');
-
-    it('Renders the Slot', () => {
-        cy.visit('storybook-static/index.html');
-        cy.get('#explorerwithvalidation--basic-usage-input').click();
+    function aliasIframe() {
         cy.get('#storybook-preview-iframe')
             .then(($iframe) => cy.wrap($iframe.contents().find('body')).as('iframe'));
+    }
 
-        input().should('have.value', '');
-    });
+    function input() {
+        aliasIframe();
+        return cy.get('@iframe').find('[data-test=input]');
+    }
 
-    it('Displays proper validation in real-time', () => {
+    function info() {
+        aliasIframe();
+        return cy.get('@iframe').find('[data-test="info"]');
+    }
+
+    function testStory(parent, story) {
         cy.visit('storybook-static/index.html');
-        cy.get('#explorerwithvalidation--basic-usage-input').click();
-        cy.get('#storybook-preview-iframe')
-            .then(($iframe) => cy.wrap($iframe.contents().find('body')).as('iframe'));
+        const storySel =
+            `#${parent.toLowerCase()}--${story.replace(/[^a-z0-9]/gi, '-').toLowerCase()}`;
+        cy.get(storySel).click();
+    }
 
-        // Default empty and invalid
-        input().should('have.value', '');
-        info().should(($info) => {
-            expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
-                valid: false,
-                touched: false,
-                required: true,
-                type: false,
-                pattern: false,
-                maxlength: false,
-                minlength: false,
-                min: false,
-                max: false,
-                step: false,
-            });
+    describe('WithValidation -> Basic Usage/input', () => {
+        beforeEach(() => testStory('WithValidation', 'Basic Usage/input'));
+
+        it('Renders the Slot', () => {
+            input().should('have.value', '');
         });
 
-        // https://github.com/cypress-io/cypress/issues/1930
-        // input().type('t');
-        // input().should('have.value', 't');
-        // info().should(($info) => {
-        //     expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
-        //         valid: false,
-        //         touched: false,
-        //         required: false,
-        //         type: false,
-        //         pattern: false,
-        //         maxlength: false,
-        //         minlength: true,
-        //         min: false,
-        //         max: false,
-        //         step: false,
-        //     });
-        // });
-    });
-
-    it('Indicates proper touched status', () => {
-        cy.visit('storybook-static/index.html');
-        cy.get('#explorerwithvalidation--basic-usage-input').click();
-        cy.get('#storybook-preview-iframe')
-            .then(($iframe) => cy.wrap($iframe.contents().find('body')).as('iframe'));
-
-        // Default empty and invalid
-        info().should(($info) => {
-            expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
-                valid: false,
-                touched: false,
-                required: true,
-                type: false,
-                pattern: false,
-                maxlength: false,
-                minlength: false,
-                min: false,
-                max: false,
-                step: false,
+        it('Displays proper validation in real-time', () => {
+            info().should(($info) => {
+                expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
+                    valid: false,
+                    touched: false,
+                    required: true,
+                    type: false,
+                    pattern: false,
+                    maxlength: false,
+                    minlength: false,
+                    min: false,
+                    max: false,
+                    step: false,
+                });
             });
+
+            // Currently broken due to
+            // https://github.com/cypress-io/cypress/issues/1930
+            // input().type('t');
+            // input().should('have.value', 't');
+            // info().should(($info) => {
+            //     expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
+            //         valid: false,
+            //         touched: false,
+            //         required: false,
+            //         type: false,
+            //         pattern: false,
+            //         maxlength: false,
+            //         minlength: true,
+            //         min: false,
+            //         max: false,
+            //         step: false,
+            //     });
+            // });
         });
 
-        // Unsure if this is the same issue as
-        // https://github.com/cypress-io/cypress/issues/1930
-        // input().focus().blur();
-        // info().should(($info) => {
-        //     expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
-        //         valid: false,
-        //         touched: true,
-        //         required: true,
-        //         type: false,
-        //         pattern: false,
-        //         maxlength: false,
-        //         minlength: false,
-        //         min: false,
-        //         max: false,
-        //         step: false,
-        //     });
-        // });
+        it('Indicates proper touched status', () => {
+            // Default empty and invalid
+            info().should(($info) => {
+                expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
+                    valid: false,
+                    touched: false,
+                    required: true,
+                    type: false,
+                    pattern: false,
+                    maxlength: false,
+                    minlength: false,
+                    min: false,
+                    max: false,
+                    step: false,
+                });
+            });
+
+            // Unsure if this is the same issue as
+            // https://github.com/cypress-io/cypress/issues/1930
+            // input().focus().blur();
+            // info().should(($info) => {
+            //     expect(JSON.parse($info[0].textContent.trim())).to.deep.equal({
+            //         valid: false,
+            //         touched: true,
+            //         required: true,
+            //         type: false,
+            //         pattern: false,
+            //         maxlength: false,
+            //         minlength: false,
+            //         min: false,
+            //         max: false,
+            //         step: false,
+            //     });
+            // });
+        });
+
     });
 
 });
